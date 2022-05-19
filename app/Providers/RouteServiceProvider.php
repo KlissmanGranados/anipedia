df32c3684b018113501297e6495cafd0785c9345
klissman <granadosklissman@gmail.com>
2022-05-19 16:11:06 -0400

    https

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvi
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
+use \Illuminate\Routing;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -37,6 +38,9 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
+
+        resolve(Routing\UrlGenerator::class)->forceScheme('https');
+        parent::boot();
     }
 
     /**
